2b51a69e05fe89a78048a1f9f62d2387f4242f07
canares12 <canarescr12@outlook.ph>
2025-05-03 21:08:01 +0800

    End of  section 3

--- API/Program.cs
+++ API/Program.cs
@@ -10,12 +10,17 @@ builder.Services.AddDbContext<AppDbContext>(options =>
 {
     options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+builder.Services.AddCors();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod()
+    .WithOrigins("http://localhost:3000","https://localhost:3000"));
+
 app.MapControllers();
 
+//seeding data
 using var scope = app.Services.CreateScope();
 var services = scope.ServiceProvider;
 var logger = services.GetRequiredService<ILogger<Program>>();
---
+++ react-client/src/App.tsx
@@ -0,0 +1,7 @@
+import { Typography } from "@mui/material";
+
+function App() {
+    return <><Typography variant='h3'>Reactivities</Typography></>    
+}
+
+export default App;
