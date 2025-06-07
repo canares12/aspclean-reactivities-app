5f4d03f7a44156420089b5e7a5ee08080a1b89b0
canares12 <canarescr12@outlook.ph>
2025-06-07 15:31:31 +0800

    End of section 6

--- react-client/src/app/layouts/App.tsx
+++ react-client/src/app/layouts/App.tsx
@@ -1,23 +1,25 @@
-import { Box, Container, CssBaseline } from "@mui/material";
+import { Box, Container, CssBaseline, Typography } from "@mui/material";
 import NavBar from "./NavBar";
 import ActivityDashboard from "../../features/activities/dashboard/ActivityDashboard";
-import { useEffect, useState } from "react";
-import axios from "axios";
+import { useState } from "react";
+import { useActivities } from "../../lib/hooks/useActivities";
 
 function App() {
-    const [activities, setActivities] = useState<Activity[]>([]);
+    //const [activities, setActivities] = useState<Activity[]>([]);
     const [selectedActivity, setSelectedActivity] = useState<Activity | undefined>(undefined);
     const [editMode, setEditMode] = useState(false);
+    const {activities, isPending} = useActivities();
 
-    useEffect(() => {
-        axios.get<Activity[]>('https://localhost:5001/api/activities')
-        .then(response => setActivities(response.data))
+    // useEffect(() => {
+    //     axios.get<Activity[]>('https://localhost:5001/api/activities')
+    //     .then(response => setActivities(response.data))
 
-        return () => {}
-    }, [])
+    //     return () => {}
+    // }, [])
+   
 
     const handleSelectActivity = (id: string) => {
-        setSelectedActivity(activities.find(x => x.id === id));
+        setSelectedActivity(activities!.find(x => x.id === id));
     }
 
     const handleCancelSelectActivity = () => {
@@ -34,27 +36,32 @@ function App() {
         setEditMode(false);
     }
 
-    const handleSubmitForm = (activity: Activity) => {
-        if(activity.id){
-            setActivities(activities.map(x => x.id === activity.id ? activity : x));
-        } else {
-            const newActivity = {...activity, id: activities.length.toString()}
-            setSelectedActivity(newActivity);
-            setActivities([...activities, newActivity]);
-        }
-        setEditMode(false);
-    }
+    // const handleSubmitForm = (activity: Activity) => {
+        // if(activity.id){
+        //     setActivities(activities.map(x => x.id === activity.id ? activity : x));
+        // } else {
+        //     const newActivity = {...activity, id: activities.length.toString()}
+        //     setSelectedActivity(newActivity);
+        //     setActivities([...activities, newActivity]);
+        // }
+    //     console.log(activity);
+    //     setEditMode(false);
+    // }
 
-    const handleDelete = (id: string) => {
-        setActivities(activities.filter(x => x.id !== id));
-    }
+    // const handleDelete = (id: string) => {
+    //     console.log(id);
+    //     // setActivities(activities.filter(x => x.id !== id));
+    // }
 
     return (
-        <Box sx={{bgcolor: '#eeeeee'}}>
+        <Box sx={{bgcolor: '#eeeeee', minHeight: '100vh'}}>
             <CssBaseline />
             <NavBar openForm={handleOpenForm} />
             <Container maxWidth='xl' sx={{mt: 3}}>
-                <ActivityDashboard 
+                {!activities || isPending ? (
+                    <Typography>Loading...</Typography>
+                ) : (
+                    <ActivityDashboard 
                     activities={activities} 
                     selectActivity={handleSelectActivity}
                     cancelSelectActivity={handleCancelSelectActivity}
@@ -62,9 +69,8 @@ function App() {
                     editMode={editMode}
                     openForm={handleOpenForm}
                     closeForm={handleCloseForm}
-                    submitForm={handleSubmitForm}
-                    deleteActivity={handleDelete}
                 />
+                )}
             </Container>
         </Box>
     );
--- react-client/src/features/activities/dashboard/ActivityCard.tsx
+++ react-client/src/features/activities/dashboard/ActivityCard.tsx
@@ -1,27 +1,56 @@
-import { Box, Button, Card, CardActions, CardContent, Chip, Typography } from "@mui/material";
+import {
+    Box,
+    Button,
+    Card,
+    CardActions,
+    CardContent,
+    Chip,
+    Typography,
+} from "@mui/material";
+import { useActivities } from "../../../lib/hooks/useActivities";
 
 type Props = {
     activity: Activity;
     selectActivity: (id: string) => void;
-    deleteActivity: (id: string) => void;
 };
 
-export default function ActivityCard({ activity, selectActivity, deleteActivity }: Props) {
+export default function ActivityCard({ activity, selectActivity }: Props) {
+    const { deleteActivity } = useActivities();
+
     return (
         <Card sx={{ borderRadius: 3 }}>
             <CardContent>
                 <Typography variant="h5">{activity.title}</Typography>
-                <Typography sx={{ color: 'text.secondary', mb: 1 }}>{activity.date.toString()}</Typography>
+                <Typography sx={{ color: "text.secondary", mb: 1 }}>
+                    {activity.date.toString()}
+                </Typography>
                 <Typography variant="body2">{activity.description}</Typography>
-                <Typography variant="subtitle1">{activity.city} / {activity.venue}</Typography>
+                <Typography variant="subtitle1">
+                    {activity.city} / {activity.venue}
+                </Typography>
             </CardContent>
-            <CardActions sx={{display: 'flex', justifyContent: 'space-between', pb: 2}}>
+            <CardActions
+                sx={{ display: "flex", justifyContent: "space-between", pb: 2 }}
+            >
                 <Chip label={activity.category} variant="outlined"></Chip>
-                <Box display='flex' gap={2}>
-                     <Button onClick={() => selectActivity(activity.id)} size="medium" variant="contained">View</Button>
-                     <Button onClick={() => deleteActivity(activity.id)} size="medium" color="error" variant="contained">Delete</Button>
+                <Box display="flex" gap={2}>
+                    <Button
+                        onClick={() => selectActivity(activity.id)}
+                        size="medium"
+                        variant="contained"
+                    >
+                        View
+                    </Button>
+                    <Button
+                        onClick={() => deleteActivity.mutate(activity.id)}
+                        loading={deleteActivity.isPending}
+                        size="medium"
+                        color="error"
+                        variant="contained"
+                    >
+                        Delete
+                    </Button>
                 </Box>
-               
             </CardActions>
         </Card>
     );
